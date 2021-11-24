57dd3c8fd34ecbda9694740d04826ca7d66f2596
Andres <andresg5997@gmail.com>
2021-11-24 21:35:25 +0100

    new pages, partial auth

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,9 +1,31 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import { loginUser, useAuthDispatch, useAuthState } from '../context';
 
 import Header from '../partials/Header';
 
 function SignIn() {
+
+  const dispatch = useAuthDispatch();
+	const { loading } = useAuthState();
+
+  const history = useHistory();
+  
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+
+	const handleSignIn = async (e) => {
+		e.preventDefault();
+
+		try {
+			let response = await loginUser(dispatch, { email, password });
+			if (!response.user) return;
+			history.push('/cameras');
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
   return (
     <div className="flex flex-col min-h-screen overflow-hidden">
 
@@ -19,7 +41,7 @@ function SignIn() {
 
               {/* Page header */}
               <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
-                <h1 className="h1">Welcome back. We exist to make entrepreneurism easier.</h1>
+                <h1 className="h1">Hello again.</h1>
               </div>
 
               {/* Form */}
@@ -28,7 +50,16 @@ function SignIn() {
                   <div className="flex flex-wrap -mx-3 mb-4">
                     <div className="w-full px-3">
                       <label className="block text-gray-800 text-sm font-medium mb-1" htmlFor="email">Email</label>
-                      <input id="email" type="email" className="form-input w-full text-gray-800" placeholder="Enter your email address" required />
+                      <input
+                        type='email'
+                        id='email'
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        disabled={loading}
+                        className="form-input w-full text-gray-800"
+                        placeholder="Enter your email address"
+                        required
+                      />
                     </div>
                   </div>
                   <div className="flex flex-wrap -mx-3 mb-4">
@@ -37,26 +68,29 @@ function SignIn() {
                         <label className="block text-gray-800 text-sm font-medium mb-1" htmlFor="password">Password</label>
                         <Link to="reset-password" className="text-sm font-medium text-blue-600 hover:underline">Having trouble signing in?</Link>
                       </div>
-                      <input id="password" type="password" className="form-input w-full text-gray-800" placeholder="Enter your password" required />
-                    </div>
-                  </div>
-                  <div className="flex flex-wrap -mx-3 mb-4">
-                    <div className="w-full px-3">
-                      <div className="flex justify-between">
-                        <label className="flex items-center">
-                          <input type="checkbox" className="form-checkbox" />
-                          <span className="text-gray-600 ml-2">Keep me signed in</span>
-                        </label>
-                      </div>
+                      <input
+                        type='password'
+                        id='password'
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        disabled={loading}
+                        className="form-input w-full text-gray-800" placeholder="Enter your password" required
+                      />
                     </div>
                   </div>
                   <div className="flex flex-wrap -mx-3 mt-6">
                     <div className="w-full px-3">
-                      <button className="btn text-white bg-blue-600 hover:bg-blue-700 w-full">Sign in</button>
+                      <button
+                        onClick={handleSignIn}
+                        disabled={loading}
+                        className="btn text-white bg-blue-600 hover:bg-blue-700 w-full"
+                      >
+                        Sign in
+                      </button>
                     </div>
                   </div>
                 </form>
-                <div className="flex items-center my-6">
+                {/* <div className="flex items-center my-6">
                   <div className="border-t border-gray-300 flex-grow mr-3" aria-hidden="true"></div>
                   <div className="text-gray-600 italic">Or</div>
                   <div className="border-t border-gray-300 flex-grow ml-3" aria-hidden="true"></div>
@@ -82,7 +116,7 @@ function SignIn() {
                       </button>
                     </div>
                   </div>
-                </form>
+                </form> */}
                 <div className="text-gray-600 text-center mt-6">
                   Don’t you have an account? <Link to="/signup" className="text-blue-600 hover:underline transition duration-150 ease-in-out">Sign up</Link>
                 </div>
